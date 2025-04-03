7cbf14d46d00b47bbe6cb4e3f9e14ddf69f53bdd
Mohammed Rayaan Pasha <mdrayaanpasha@gmail.com>
2025-04-03 20:52:59 +0530

    i guess unmessed?

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const GlassNav = () => {
   const [isOpen, setIsOpen] = useState(false);
